32a52a801094d47520dd6dd7899b070985c9c4e4
luizcampos331 <luizcampos331@gmail.com>
2021-07-17 21:42:31 -0300

    Challenge finished

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,17 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      console.log(user_id);
+      const user = this.turnUserAdminUseCase.execute({
+        user_id: user_id?.toString() || "",
+      });
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
